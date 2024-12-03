8c5fc96558a021477826b94ad7bbfc24049ea302
Syllos <joaohenrique.braga@outlook.com>
2024-12-02 22:13:04 -0300

    Codigo base 100% pronto

--- dado.py
+++ dado.py
@@ -11,30 +11,30 @@ def datatotal():
     data = [
         ["Meses", "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC", "Total"],
         
-        ["Receita 1.10", 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0, 0, 0],
+        ["Receita - 1.10", 0, 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0, 0, 0],
         
-        [2.01],
+        ["2.01 - Prolabore", 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0, 0, 0, 0 ],
         ["Prolabore", 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0, 0, 0, 0 ],
         ["IRRF/INSS prolabore", 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0, 0, 0, 0 ],
         ["Retirada Ana Paula Syllos Braga", 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0, 0, 0, 0 ],
         ["Retirada Sérgio Augusto Braga", 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0, 0, 0, 0 ],
 
-        [2.03],
+        ["2.03 - Honorários Contabeis", 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0, 0, 0, 0 ],
         ["Honorários Contabeis", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
-        [2.04, "", "", "", "", "", "", "", "", "", "", "", "", 0],
+        ["2.04 - Materiais", 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0, 0, 0, 0 ],
         ["Material de informatica", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],         
         ["Material de escritório", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
-        [2.05, "", "", "", "", "", "", "", "", "", "", "", "", 0],
+        ["2.05 - Impostos", 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0, 0, 0, 0 ],
         ["ISS retido", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
         ["SIMPLES NACIONAL", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         ["Outras taxas e impostos", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
-        [2.06],
+        ["2.06 - Despesas Viagem", 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0, 0, 0, 0 ],
         ["Despesa de viagem", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
-        [2.07, "", "", "", "", "", "", "", "", "", "", "", "", 0],
+        ["2.07 - Veiculo", 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0, 0, 0, 0 ],
         ["Estacionamento", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #19
         ["Combustivel", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         ["Lavagem de carro", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -42,16 +42,16 @@ def datatotal():
         ["IPVA carro", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         ["Pneus", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
-        [2.08],
+        ["2.08 - Alimentação", 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0, 0, 0, 0 ],
         ["Alimentação", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
-        [2.09],
+        ["2.09 - Saúde", 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0, 0, 0, 0 ],
         ["Plano de Saúde", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
-        [2.50],
-        ["Emprestimos Socios", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ["2.50 - Emprestimo Sócios", 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0, 0, 0, 0 ],
+        ["Emprestimos Sócios", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
-        [2.52],
+        ["2.52 - Pagamento Sócios", 0, 0, 0, 0, 0, 0 ,0 ,0, 0, 0, 0, 0, 0 ],
         ["Pagamento Sócios", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
 
@@ -89,51 +89,69 @@ def datatotal():
 
         elif classe == 2.03:
             setMonth(comp, row, data, 8)
+            setMonth(comp, row, data, 7)
 
         elif classe == 2.04:
             match str(row["Descrição"]).lower():
                 case "material de informatica":
                     setMonth(comp, row, data, 10)
+                    setMonth(comp, row, data, 9)
                 case "material de escritório":
                     setMonth(comp, row, data, 11)
+                    setMonth(comp, row, data, 9)
 
         elif classe == 2.05:
             match row["Descrição"].lower():
                 case "iss retido":
                     setMonth(comp, row, data, 13)
+                    setMonth(comp, row, data, 12)
                 case "simples nacional":
                     setMonth(comp, row, data, 14)
+                    setMonth(comp, row, data, 12)
                 case "outras taxas e impostos":
                     setMonth(comp, row, data, 15)
+                    setMonth(comp, row, data, 12)
 
         elif classe == 2.06:
             setMonth(comp, row, data, 17)
+            setMonth(comp, row, data, 16)
 
         elif classe == 2.07:
             match row["Descrição"].lower():
                 case "estacionamento":
                     setMonth(comp, row, data, 19)
+                    setMonth(comp, row, data, 18)
                 case "combustível":
                     setMonth(comp, row, data, 20)
+                    setMonth(comp, row, data, 18)
                 case "lavagem de carro":
                     setMonth(comp, row, data, 21)
+                    setMonth(comp, row, data, 18)
                 case "manurtenção":
                     setMonth(comp, row, data, 22)
+                    setMonth(comp, row, data, 18)
                 case "manutenção":
                     setMonth(comp, row, data, 22)
+                    setMonth(comp, row, data, 18)
                 case "ipva carro":
                     setMonth(comp, row, data, 23)
+                    setMonth(comp, row, data, 18)
                 case "pneus":
                     setMonth(comp, row, data, 24)
+                    setMonth(comp, row, data, 18)
 
         elif classe == 2.08:
             setMonth(comp, row, data, 26)
+            setMonth(comp, row, data, 25)
         elif classe == 2.09:
             setMonth(comp, row, data, 28)
+            setMonth(comp, row, data, 27)
         elif classe == 2.50:
             setMonth(comp, row, data, 30)
+            setMonth(comp, row, data, 29)
         elif classe == 2.51:
             setMonth(comp, row, data, 32)
+            setMonth(comp, row, data, 31)
 
     data[1][13] += sum(data[1][1:12])
 
@@ -143,8 +161,12 @@ def datatotal():
 
     data[3][13] += totalProlabore(data)
 
+    #honorarios
     data[8][13] += sum(data[8][1:12])
+    data[7][13] += sum(data[7][1:12])
 
+    #materiais
+    data[8][13] += sum(data[10][1:12]) + sum(data[11][1:12])
     data[9][13] += sum(data[10][1:12]) + sum(data[11][1:12])
     data[10][13] += sum(data[10][1:12])
     data[11][13] += sum(data[11][1:12])
@@ -154,6 +176,7 @@ def datatotal():
     data[14][13] += sum(data[14][1:12])
     data[15][13] += sum(data[15][1:12])
 
+    data[16][13] += sum(data[17][1:12])
     data[17][13] += sum(data[17][1:12])
 
     data[18][13] += sum(data[19][1:12]) + sum(data[20][1:12]) + sum(data[21][1:12]) + sum(data[22][1:12]) + sum(data[23][1:12]) + sum(data[24][1:12])
@@ -164,12 +187,16 @@ def datatotal():
     data[23][13] += sum(data[23][1:12])
     data[24][13] += sum(data[24][1:12])
 
+    data[25][13] += sum(data[26][1:12])
     data[26][13] += sum(data[26][1:12])
 
+    data[27][13] += sum(data[28][1:12])
     data[28][13] += sum(data[28][1:12])
 
+    data[29][13] += sum(data[30][1:12])
     data[30][13] += sum(data[30][1:12])
 
+    data[31][13] += sum(data[32][1:12])
     data[32][13] += sum(data[32][1:12])
 
     for x in data:
